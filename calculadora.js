5bbea641de48ab8909b09a757a6961c820657832
José Morais <josemlneto2905@gmail.com>
2024-05-22 13:45:11 -0300

    terceiro commit

--- calculadora.js
+++ calculadora.js
@@ -3,4 +3,6 @@ let message = prompt("Olá mundo?")
 if(message == "sim"){
     const body = document.querySelector("main")
     body.innerHTML="OLÁ MUNDO!"
+}else if(message == "não"){
+    alert("to nem ai")
 }
